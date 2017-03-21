2154c55ac1abd565ed618f6a65f8f65f08f09e61
Rob <robjepr.nl>
2017-03-21 21:04:52 +0100

    Create clock component and missing karma.conf

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Clock = require('Clock');
+
+var Countdown = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <Clock totalSeconds={129}/>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Countdown;
