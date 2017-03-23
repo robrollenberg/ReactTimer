b586937bedd0e81fe66e0296c0e1a11a87ab41d1
Rob <robjepr.nl>
2017-03-23 20:58:46 +0100

    Final Countdown part

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -20,10 +20,14 @@ var Countdown = React.createClass({
           this.setState({count: 0});
         case 'paused':
           clearInterval(this.timer)
-          this.timer = undefnined;
+          this.timer = undefined;
           break;
       }
     }
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
   },
   startTimer: function () {
     this.timer = setInterval(() => {
@@ -31,6 +35,10 @@ var Countdown = React.createClass({
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   },
   handleSetCountdown: function (seconds) {
@@ -54,6 +62,7 @@ var Countdown = React.createClass({
 
     return (
       <div>
+        <h1 className="page-title">Countdown App</h1>
         <Clock totalSeconds={count}/>
         {renderControlArea()}
       </div>
